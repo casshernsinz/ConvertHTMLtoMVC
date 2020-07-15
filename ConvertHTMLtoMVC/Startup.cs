d5bae59088315fb2504b75841ca58c05e28f0e9f
casshernsinz <66576864+casshernsinz@users.noreply.github.com>
2020-07-15 08:51:59 -0500

    Add project files.

---
+++ ConvertHTMLtoMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConvertHTMLtoMVC.Startup))]
+namespace ConvertHTMLtoMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
